2d4ff39e456788df4aa940c1d563aed4e45838c2
Christian Lang <christianl@6wunderkinder.com>
2013-04-17 17:43:15 +0200

    * pass app name as parameter to the console app

--- ClickOnceUninstaller/Program.cs
+++ ClickOnceUninstaller/Program.cs
@@ -7,20 +7,26 @@ namespace Wunder.ClickOnceUninstaller
 
         static void Main(string[] args)
         {
-            var uninstallInfo = UninstallInfo.Find("Wunderlist");
+            if (args.Length != 1 || string.IsNullOrEmpty(args[0]))
+            {
+                Console.WriteLine("Usage:\nClickOnceUninstaller appName");
+                return;
+            }
 
+            var appName = args[0];
+
+            var uninstallInfo = UninstallInfo.Find(appName);
             if (uninstallInfo == null)
             {
-                Console.WriteLine("Application not found");
+                Console.WriteLine("Could not find application \"{0}\"", appName);
+                return;
             }
-            else
-            {
-                var uninstaller = new Uninstaller();
-                uninstaller.Uninstall(uninstallInfo);
-                Console.WriteLine("Uninstall complete");
-            }
-            
-            Console.ReadLine();
+
+            Console.WriteLine("Uninstalling application \"{0}\"", appName);
+            var uninstaller = new Uninstaller();
+            uninstaller.Uninstall(uninstallInfo);
+
+            Console.WriteLine("Uninstall complete");
         }
     }
 }
